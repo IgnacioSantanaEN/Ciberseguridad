dc128adbc85427a7000ab6448fdd02ade7488235
IgnacioSantanaEN <ig.santanan@duocuc.cl>
2026-06-29 09:57:57 -0400

    Entorno virtual y app para OWASP/ZAP

--- app.py
+++ app.py
@@ -26,3 +26,6 @@ print(f"Procesador    : {procesador}")
 print("===================================")
 print("Programa ejecutado correctamente")
 print("===================================")
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=5000)
